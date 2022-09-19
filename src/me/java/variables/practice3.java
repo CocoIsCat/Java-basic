12c879cddd8e09839fd6d162061957b8929573e1
cocoiscat <demy176@naver.com>
2022-09-19 21:42:06 +0900

    feat: practice 3

---
+++ src/me/java/variables/practice3.java
@@ -0,0 +1,10 @@
+package me.java.variables;
+
+public class practice3 {
+    public static void main(String[] args) {
+        int a = 10;
+        System.out.println(a);  //변수 a에 있는 리터럴 출력
+        System.out.println('a');    //문자 a 출력
+        System.out.println("a");    //문자열 a 출력
+    }
+}
